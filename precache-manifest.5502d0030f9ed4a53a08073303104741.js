56f5ba4b42697a5892b4704f4a247d04fac687e8
pk <pk@rsoftstudio.pl>
2019-01-28 11:32:20 +0100

    Updates

---
+++ precache-manifest.5502d0030f9ed4a53a08073303104741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/filteredlist1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a16627cbaab7beeae2a1",
+    "url": "/filteredlist1/static/js/runtime~main.a16627cb.js"
+  },
+  {
+    "revision": "47a83a3c7509b0f27125",
+    "url": "/filteredlist1/static/js/main.47a83a3c.chunk.js"
+  },
+  {
+    "revision": "3f0b22993c54ddcf39f8",
+    "url": "/filteredlist1/static/js/1.3f0b2299.chunk.js"
+  },
+  {
+    "revision": "47a83a3c7509b0f27125",
+    "url": "/filteredlist1/static/css/main.44b1c4e0.chunk.css"
+  },
+  {
+    "revision": "6ee6e2b2dd19674fa10d40b44bdaab4f",
+    "url": "/filteredlist1/index.html"
+  }
+];
